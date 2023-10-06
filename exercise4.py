9c2ffc11ca2436b480ace7b9908e700adc0a570f
Jhasija9 <91153340+Jhasija9@users.noreply.github.com>
2023-10-06 18:08:09 -0400

    Add files via upload

--- exercise4.py
+++ exercise4.py
@@ -1,37 +1,59 @@
 import numpy as np
 import pandas as pd
 from sklearn.datasets import load_iris
-from sklearn import linear_model
-from sklearn.metrics import accuracy_score, classification_report
-from sklearn.model_selection import train_test_split
-from numpy import genfromtxt
-from sklearn.model_selection import KFold
-from sklearn.model_selection import cross_val_score
-from numpy import mean
-from numpy import std
-from sklearn import tree
-print("\n Fold Cross Validation Approach Logistic Regression on the Iris DataSet -> ")
-irisDataset =genfromtxt('IrisNew.csv', delimiter=',',dtype=None,encoding=None)
-x = pd.DataFrame(irisDataset[1:,:4])
-y = pd.DataFrame(irisDataset[1:,4]).values.flatten()
-# Splitting the dataset for training and testing 
-x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.3, random_state=12)
+from sklearn.model_selection import KFold, cross_val_score
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+import matplotlib.pyplot as plt
+iris = load_iris()
+X = iris.data
+y = iris.target
+kf = KFold(n_splits=5, random_state=1, shuffle=True)
+logreg_scores = []
+clf_scores = []
 
-irisDatasetModel = linear_model.LogisticRegression(multi_class='auto', solver='lbfgs', max_iter=1000)
-c = KFold(n_splits=5, random_state=1, shuffle=True)
-scores = cross_val_score(irisDatasetModel, x, y, scoring='accuracy', cv=c, n_jobs=-1)
-# report performance
-print('The mean accuracy of the model is',mean(scores))
-print('The Standard Deviation accuracy of the model is',std(scores))
+print("\nFold Cross Validation Approach Logistic Regression -> ")
+logreg = LogisticRegression(multi_class='auto', solver='lbfgs', max_iter=1000)
 
-print("\n Fold Cross Validation Approach Decision Tree Model on the Iris DataSet -> ")
+for train_index, test_index in kf.split(X):
+    X_train, X_test = X[train_index], X[test_index]
+    y_train, y_test = y[train_index], y[test_index]
+    
+    logreg.fit(X_train, y_train)
+    y_pred = logreg.predict(X_test)
+    accuracy = np.mean(y_pred == y_test)
+    logreg_scores.append(accuracy)
+    logreg_std_dev = np.std(logreg_scores)
+    logreg_mean = np.mean(logreg_scores)
 
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=12)
-clf = tree.DecisionTreeClassifier()
-clf.fit(x_train,y_train)
-scores = cross_val_score(clf, x, y, scoring='accuracy', cv=c, n_jobs=-1)
-print('The mean accuracy of the model is',mean(scores))
-print('The Standard Deviation accuracy of the model is',std(scores))
+print("\nFold Cross Validation Approach Decision Tree Model -> ")
+clf = DecisionTreeClassifier()
 
+for train_index, test_index in kf.split(X):
+    X_train, X_test = X[train_index], X[test_index]
+    y_train, y_test = y[train_index], y[test_index]
+    
+    clf.fit(X_train, y_train)
+    y_pred = clf.predict(X_test)
+    accuracy = np.mean(y_pred == y_test)
+    clf_scores.append(accuracy)
+    clf_std_dev = np.std(clf_scores)
+    clf_mean = np.mean(clf_scores)
+
+print('Standard Deviation of Accuracy (Logistic Regression): {:.2f}'.format(logreg_std_dev))
+print('Standard Deviation of Accuracy (Decision Tree): {:.2f}'.format(clf_std_dev))
+print('Mean Accuracy (Logistic Regression): {:.2f}'.format(logreg_mean))
+print('Mean Accuracy (Decision Tree): {:.2f}'.format(clf_mean))
+
+# Plot the accuracy curve
+plt.figure(figsize=(10, 6))
+plt.plot(range(1, 6), logreg_scores, marker='o', linestyle='-', color='b', label='Logistic Regression')
+plt.plot(range(1, 6), clf_scores, marker='o', linestyle='-', color='g', label='Decision Tree')
+plt.title("Accuracy vs. Fold (Logistic Regression vs. Decision Tree)")
+plt.xlabel("Fold")
+plt.ylabel("Accuracy")
+plt.legend()
+plt.grid(True)
+plt.show()
 
 
